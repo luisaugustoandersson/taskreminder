a5a56972a7d7d560db62bf7d7f2b398904bc9eb1
luis <luis@nb-luis>
2013-06-28 22:22:06 -0300

    projeto limpo

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/luis/NetBeansProjects/DOOIII/taskreminder/bin/AndroidManifest.xml : \
-/home/luis/NetBeansProjects/DOOIII/taskreminder/AndroidManifest.xml \
--- bin/TaskReminder-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/luis/NetBeansProjects/DOOIII/taskreminder/bin/TaskReminder-debug-unaligned.apk : \
-/home/luis/NetBeansProjects/DOOIII/taskreminder/bin/TaskReminder.ap_ \
-/home/luis/NetBeansProjects/DOOIII/taskreminder/bin/classes.dex \
-/home/luis/Documentos/AndroidSDK/tools/support/annotations.jar \
